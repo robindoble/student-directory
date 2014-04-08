462cb5a8347ad0e6e9bb5b9f9dcd9708b3e4956a
Robin <robindoble@gmai.com>
2014-04-08 15:34:43 +0100

    Refactored code into multiple new methods and added an instance variable that can be used throughout the program

--- directory_pro.rb
+++ directory_pro.rb
@@ -1,8 +1,10 @@
+#declare students an empty array variable accessible through the entire file
+@students=[]
 
 #student_fixed_list avoids manual input of student data
-student_fixed_list = [{:name => "Adam",:cohort => :May},{:name => "Anthony",:cohort => :May},{:name => "Peter",:cohort => :May},
-	{:name => "Robin",:cohort => :May},{:name => "Bethsheeba",:cohort => :Feb},{:name => "MarcusAnthony",:cohort => :Feb},
-	{:name => "Sarah-Ann",:cohort => :Feb}]
+# student_fixed_list = [{:name => "Adam",:cohort => :May},{:name => "Anthony",:cohort => :May},{:name => "Peter",:cohort => :May},
+# 	{:name => "Robin",:cohort => :May},{:name => "Bethsheeba",:cohort => :Feb},{:name => "MarcusAnthony",:cohort => :Feb},
+# 	{:name => "Sarah-Ann",:cohort => :Feb}]
 
 #create a neat program header
 def print_header
@@ -14,8 +16,6 @@ end
 #now includes other student attributes of name, email and skype id
 def input_students
 	puts "Please enter the first name of the students(s)"
-	#create an empty array
-	students = []
 	#get the first name
 	name = gets.chomp
 	#while the name is not empty prompt for other info and repeat proccess
@@ -27,55 +27,64 @@ def input_students
 		# puts "Please enter the skype_id of the students(s)"
 		# skype_id=gets.chomp
 		# # add the student data hash to the array
-		students << {name: name, email: "email@gmail.com", skype_id: "skype_id"}
-		puts "Now we have #{students.length} students"
+		@students << {name: name, email: "email@gmail.com", skype_id: "skype_id"}
+		puts "Now we have #{@students.length} students"
 		# get another name from the user
 		puts "Please enter the next name of the students(s), or hit enter to finish"		
 		name = gets.chomp
 	end
 	# return the array of students
-	students
+	@students
 end
 
 	
-def print(student)
+def print_students_list
 		i = 0
-		student.each do |element|
+		@students.each do |element|
 		puts "#{i += 1}. #{element[:name]}, #{element[:email]}, #{element[:skype_id]}"
 		end
 end
 
 
-def print_footer(student)
-	puts "Overall, we have #{student.length} great student(s)"
+def print_footer
+	puts "Overall, we have #{@students.length} great student(s)"
 end
 
-def interactive_menu
-		students = []
-	loop do
-		#1. print menu and ask user what to do
-			puts "1. Input the students"
-			puts "2. Show the students"
-			puts "9. Exit"
-		#2. read input and assign to a variable
-			selection = gets.chomp
-		#3. do what user has asked
-			case selection
-			when "1"
-			#input students
-			students = input_students
-			when "2"
-			#show students
-			print_header
-			print(students)
-			print_footer(students)
-			when "9"
-			exit #will exit the program
-			else 
-			puts "I don't know what you mean, please try again"
-			end
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def process(selection)
+	case selection
+	when "1"
+	#input students
+	@students = input_students
+	when "2"
+	#show students
+	show_students
+	when "9"
+	exit #will exit the program
+	else 
+	puts "I don't know what you mean, please try again"
 	end
 end
 
-2
+def interactive_menu
+		# students = []
+	loop do
+		#1. print menu and ask user what to do
+			print_menu
+		#2. take user selection and run method process 
+			process(gets.chomp)
+	end
+end
+
 interactive_menu
